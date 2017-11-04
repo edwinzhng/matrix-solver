5b4d04890388dff13f56550f56c978b5e7f6ee09
Edwin Zhang <edwinzhang64@gmail.com>
2017-11-04 01:44:46 -0400

    REF complete

--- gauss.c
+++ gauss.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+// steps for row echelon form
 int ge_fw(float *matrix, int rows, int cols) {
   int pivotRow = 0; // set row that the pivot is on
   for(int currentCol = 0; currentCol < cols; currentCol++) {
@@ -34,20 +35,27 @@ int ge_fw(float *matrix, int rows, int cols) {
       for(int i = pivotRow + 1; i < rows; i++) {
         if(matrix[i * cols + currentCol] != 0) {
           float r = matrix[i * cols + currentCol] / matrix[pivotRow * cols + currentCol];
-          printf("%f\n", r);
           // subtract for each individual column in a row
           for(int j = 0; j < cols; j++) {
-            matrix[i * cols + j] - r * matrix[pivotRow * cols + currentCol];
+            for(int r = 0; r < rows; r++) {
+              for(int c = 0; c < cols; c++) {
+                printf("%.1f ", matrix[r * cols + c]);
+              }
+              printf("\n");
+            }
+            printf("\n");
+
+            matrix[i * cols + j] -= r * matrix[pivotRow * cols + j];
           }
         }
       }
       pivotRow++;
     }
-
   }
   return 1;
 }
 
+// steps for reduced row echelon form
 int ge_bw(float *matrix, int rows, int cols, float *matrix_out) {
 
   return 1;
@@ -56,11 +64,12 @@ int ge_bw(float *matrix, int rows, int cols, float *matrix_out) {
 int main()
 {
   int rows = 4, cols = 2;
-  float *matrix[4][2] = { {0, 1}, {1, 2}, {3, 4}, {5, 5} };
+  float matrix[4][2] = { {0.0f, 1.0f}, {1.0f, 2.0f}, {3.0f, 4.0f}, {5.0f, 5.0f} };
   ge_fw(matrix, rows, cols);
-  for(int i = 0; i < rows; i++) {
-    for(int j = 0; j < cols; j++) {
-      printf("%d ", matrix[i][j]);
+  // ge_bw(matrix, rows, cols);
+  for(int r = 0; r < rows; r++) {
+    for(int c = 0; c < cols; c++) {
+      printf("%.1f ", matrix[r][c]);
     }
     printf("\n");
   }
